19d91fef3a33da9e82158fe29c39689b61c0679b
Ashish Kaintura <ashu88793@gmail.com>
2026-01-12 16:10:21 +0530

    new banner

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -32,7 +32,7 @@ const slides = [
 
 export default function HeroSlider() {
   return (
-    <div className="w-full sm:h-[100vh] h-64 sm:mt-0 mt-[73px] relative">
+    <div className="w-full sm:h-screen h-64 sm:mt-0 mt-[73px] relative">
       <Swiper
         modules={[Autoplay, Pagination, Navigation, EffectCreative]}
         effect="creative"
@@ -52,12 +52,12 @@ export default function HeroSlider() {
         pagination={{ clickable: true }}
         navigation
         speed={1000} // smooth transition
-        className="h-full"
+        className="h-screen"
       >
         {slides.map((slide) => (
           <SwiperSlide key={slide.id}>
             <div
-              className="h-full w-full bg-center bg-cover"
+              className="h-screen  bg-center bg-cover  bg-no-repeat"
               style={{ backgroundImage: `url(${slide.image})` }}
             >
               <div className="absolute inset-0 bg-black/10" />
